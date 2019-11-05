185c65186b4dd092e084a772ec4b779453e4ed3e
kesihinz <keishin.chou@gmail.com>
2019-11-05 19:24:19 +0900

    Create Person class and Person cell in IB.

---
+++ FacesToNames/PersonCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonCell.swift
+//  FacesToNames
+//
+//  Created by Keishin CHOU on 2019/11/05.
+//  Copyright © 2019 Keishin CHOU. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
